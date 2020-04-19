a6c27d436c45153842d5fea00b516b9e8590e0b6
findxy <findxy@qq.com>
2020-04-19 15:12:55 +0800

    1

---
+++ 远程工程/first/1.3.cpp
@@ -0,0 +1,14 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "glad.h"
+#include "glfw3.h"
+#include "stb_image.h"
+#include "test_texture1.hpp"
+#include "shader_s.h"
+//#include "test_texture2.hpp"
+#include <iostream>
+
+int main()
+{
+    test_texture11();
+//    test_texture22();
+}
