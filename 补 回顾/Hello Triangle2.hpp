06c778849e56e414842ccac343d5a72056116e9b
findxy <findxy@qq.com>
2020-04-20 15:25:53 +0800

    更新

---
+++ 补 回顾/Hello Triangle2.hpp
@@ -0,0 +1,14 @@
+//
+//  Hello Triangle2.hpp
+//  first
+//
+//  Created by Admin on 2020/4/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+#ifndef Hello_Triangle2_hpp
+#define Hello_Triangle2_hpp
+
+int Hello_Triangle2();
+
+#endif /* Hello_Triangle2_hpp */
--- 补 回顾/first/main.cpp
+++ 补 回顾/first/main.cpp
@@ -1,11 +1,13 @@
 #define STB_IMAGE_IMPLEMENTATION
 
 //#include "Hello Window.hpp"
-#include "Hello Triangle.hpp"
+//#include "Hello Triangle.hpp"
+#include "Hello Triangle2.hpp"
 #include <iostream>
 
 int main()
 {
-//   Hello_Window();
-    Hello_Triangle();
+//    Hello_Window();
+//    Hello_Triangle();
+    Hello_Triangle2();
 }
