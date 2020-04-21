286f4d580e71f5ec7365c3690e7a3808d4f93899
findxy <findxy@qq.com>
2020-04-21 16:23:08 +0800

    更新

--- 自己写的代码/Test_Star.cpp
+++ 自己写的代码/Test_Star.cpp
@@ -33,24 +33,24 @@ int Test_Star()
         "}\n\0";
     
     float vertices[] = {
-        0.81f,0.3f,0.0f, //K 0
-        0.19f,0.3f,0.0f,//L 1
-        0.0f,0.78f,0.0f,//M 2
-        -0.19f,0.3f,0.0f,//N 3
-        -0.78f,0.3f,0.0f,//O 4
-        -0.3f,0.0f,0.0f,//P 5
-        0.31f,-0.01f,0.0f,//Q 6
-        -0.5f,-0.5f,0.0f,//R 7
-        0.01f,-0.19f,0.0f,//S 8
-        0.5f,-0.5f,0.0f,//T 9
+        0.0,0.06,0.0f, //F 0
+        0.04f,0.02f,0.0f,//G 1
+        0.03f,-0.04f,0.0f,//H 2
+        -0.03f,-0.04f,0.0f,//I 3
+        -0.04f,0.02f,0.0f,//J 4
+        -0.01f,0.02f,0.0f,//K 5
+        0.01f,0.02f,0.0f,//L 6
+        0.02f,0.0f,0.0f,//M 7
+        0.0f,-0.0155f,0.0f,//N 8
+        -0.02f,0.0f,0.0f,//O 9
     };
 
     unsigned int indices[] = { // 注意索引从0开始!
-        2,5,6,
-        0,3,8,
-        9,5,1,
-        7,3,6,
-        4,8,1,
+        0,9,7,
+        1,5,8,
+        2,6,9,
+        3,5,7,
+        4,6,8,
         
     };
 
@@ -84,64 +84,88 @@ int Test_Star()
     
     
     unsigned int VBO,VAO,EBO ;
-    glGenBuffers(1, &VBO);
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1,&EBO);
-    glBindVertexArray(VAO);
-    
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);//忽略了解绑
-    glBindVertexArray(0);//忽略了解绑
-// ------------------------------------
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    
-    int  success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    
-    if(!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }else
-    {
-        std::cout<<"yes"<<std::endl;
-    }
-    
- // ------------------------------------
-    
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    
-//--------------------------------------
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    
-//    glUseProgram(shaderProgram);
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-    
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
     
 
     
+
+    
+    float step=0.01;
+    bool flog=true;
     while(!glfwWindowShouldClose(window))
     {
+        if(flog)
+        {
+            for(int i=0;i<30;i=i+3)
+            {
+                vertices[i]+=step;
+                if(vertices[i]>1)
+                    flog=false;
+            }
+        }
+        
+        glGenBuffers(1, &VBO);
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1,&EBO);
+        glBindVertexArray(VAO);
+        
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);//忽略了解绑
+        glBindVertexArray(0);//忽略了解绑
+        
+    
+    // ------------------------------------
+        unsigned int vertexShader;
+        vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+        glCompileShader(vertexShader);
+        
+        int  success;
+        char infoLog[512];
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+        
+        if(!success)
+        {
+            glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }else
+        {
+            std::cout<<"yes"<<std::endl;
+        }
+        
+     // ------------------------------------
+        
+        unsigned int fragmentShader;
+        fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+        glCompileShader(fragmentShader);
+        
+    //--------------------------------------
+        unsigned int shaderProgram;
+        shaderProgram = glCreateProgram();
+            
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+        
+        
+        //    glUseProgram(shaderProgram);
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+        
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        
+        
+        
+        
+        
+        
+        
         processInput(window);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
