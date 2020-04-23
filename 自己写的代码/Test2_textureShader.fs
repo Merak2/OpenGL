0167765d9a0a5a097ebb4239a89a77c2b64917a5
findxy <findxy@qq.com>
2020-04-23 20:13:37 +0800

    Test2

--- 自己写的代码/Test2_textureShader.fs
+++ 自己写的代码/Test2_textureShader.fs
@@ -12,6 +12,8 @@ void main()
 //    FragColor = texture(ourTexture, TexCoord)*vec4(0.0f, 0.1058f, 0.445f, 1.0f);
     lowp vec4 col = texture(ourTexture, TexCoord);
     FragColor = vec4(col.r*0.299+col.g*0.587+col.b*0.114,col.r*0.299+col.g*0.587+col.b*0.114,col.r*0.299+col.g*0.587+col.b*0.114,1.0f);
+    //rgb转灰度 https://blog.csdn.net/xiaxl/article/details/72622236
+    //
 //    FrageColor = vec4(1.0f,1.0f,1.0f,1.0f);
     
 }
